032e3bbfe8542d477600507aa23e3d4174fcf964
Vladimir Volosnikov <volosnikovvladimir69@gmail.com>
2025-01-22 21:00:30 +0500

    Create model Order; add property total_price

--- online_cafe/orders/models.py
+++ online_cafe/orders/models.py
@@ -1,3 +1,25 @@
+"""Module with ORM models."""
+
 from django.db import models
 
-# Create your models here.
+from menu.models import Dish
+
+
+class Order(models.Model):
+    """ORM representation of the table of orders."""
+
+    id = models.IntegerField(primary_key=True)
+    table_number = models.IntegerField(
+        null=False,
+        help_text="Номер стола"
+    )
+    items = models.ManyToManyField(Dish, help_text="Список блюд в заказе")
+    status = models.CharField(
+        choices=("в ожидании", "готово", "оплачено"),
+        help_text="Статус заказа: “в ожидании”, “готово”, “оплачено”"
+    )
+
+    @property
+    def total_price(self) -> float:
+        """Get total price."""
+        return sum(self.items_set.values_list("price", flat=True))
