782303759e743b9045fc82834cc6fb7027bec9ca
Vladimir Volosnikov <volosnikovvladimir69@gmail.com>
2025-01-23 16:19:29 +0500

    Replase help_text to verbose_name

--- online_cafe/orders/models.py
+++ online_cafe/orders/models.py
@@ -8,15 +8,15 @@ from menu.models import Dish
 class Order(models.Model):
     """ORM representation of the table of orders."""
 
-    table_number = models.IntegerField(null=False, help_text="Номер стола", validators=[MinValueValidator(0)])
-    items = models.ManyToManyField(Dish, help_text="Список блюд в заказе")
+    table_number = models.IntegerField(null=False, verbose_name="Номер стола", validators=[MinValueValidator(0)])
+    items = models.ManyToManyField(Dish, verbose_name="Список блюд в заказе")
     status = models.CharField(
         choices={
             "pending": "в ожидании",
             "done": "готово",
             "paid": "оплачено",
         },
-        help_text="Статус заказа: “в ожидании”, “готово”, “оплачено”",
+        verbose_name="Статус заказа",
         default="в ожидании"
     )
 
