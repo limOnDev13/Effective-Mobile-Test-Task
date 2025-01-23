aab0a77eff0aec6981ddc19486195c0269e3aa79
Vladimir Volosnikov <volosnikovvladimir69@gmail.com>
2025-01-23 12:56:57 +0500

    Add view classes OrderCreateView, OrderListView

--- online_cafe/orders/views.py
+++ online_cafe/orders/views.py
@@ -1,1 +1,18 @@
 """Class with Views."""
+
+from django.views.generic import CreateView, ListView
+from django.urls import reverse_lazy
+
+from .models import Order
+
+
+class OrderCreateView(CreateView):
+    model = Order
+    fields = "table_number", "items"
+    success_url = reverse_lazy("orders:orders_list")
+    template_name = "orders/orders-create.html"
+
+
+class OrderListView(ListView):
+    queryset = Order.objects.prefetch_related("items")
+    template_name = "orders/orders-list.html"
