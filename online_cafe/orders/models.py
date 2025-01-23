c6e6083c5fdeeb435f612e60d11171a17aaf22e1
Vladimir Volosnikov <volosnikovvladimir69@gmail.com>
2025-01-23 15:32:52 +0500

    Fix choices in field status; fix property total_price;

--- online_cafe/orders/models.py
+++ online_cafe/orders/models.py
@@ -8,11 +8,14 @@ from menu.models import Dish
 class Order(models.Model):
     """ORM representation of the table of orders."""
 
-    id = models.IntegerField(primary_key=True)
     table_number = models.IntegerField(null=False, help_text="Номер стола", validators=[MinValueValidator(0)])
     items = models.ManyToManyField(Dish, help_text="Список блюд в заказе")
     status = models.CharField(
-        choices=("в ожидании", "готово", "оплачено"),
+        choices={
+            "pending": "в ожидании",
+            "done": "готово",
+            "paid": "оплачено",
+        },
         help_text="Статус заказа: “в ожидании”, “готово”, “оплачено”",
         default="в ожидании"
     )
@@ -20,4 +23,4 @@ class Order(models.Model):
     @property
     def total_price(self) -> float:
         """Get total price."""
-        return sum(self.items_set.values_list("price", flat=True))
+        return sum(self.items.values_list("price", flat=True))
