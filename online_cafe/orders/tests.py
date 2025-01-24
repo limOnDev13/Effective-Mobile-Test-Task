7cb3d8b2edeb01d694dbd0743c63f45efc1564a2
Vladimir Volosnikov <volosnikovvladimir69@gmail.com>
2025-01-24 21:53:10 +0500

    Add test for OrderListView

--- online_cafe/orders/tests.py
+++ online_cafe/orders/tests.py
@@ -1,1 +1,24 @@
 """Module with tests."""
+
+from django.test import TestCase
+from django.urls import reverse
+from .models import Order
+
+
+class OrderListViewTest(TestCase):
+    """Test case class for testing OrderListView."""
+
+    fixtures = [
+        "../fixtures/dishes-fixture.json",
+        "../fixtures/orders-fixture.json",
+    ]
+
+    def test_orders(self):
+        """Test getting list of orders."""
+        response = self.client.get(reverse("orders:orders-list"))
+
+        self.assertQuerySetEqual(
+            qs=Order.objects.order_by("pk").all(),
+            values=sorted((s.pk for s in response.context["orders"])),
+            transform=lambda p: p.pk,
+        )
