6399b7fab5a483c4f348cca59bdc766cd08b03ec
Vladimir Volosnikov <volosnikovvladimir69@gmail.com>
2025-01-24 13:23:04 +0500

    Add urlpatterns for OrderDetailView, OrderUpdateView, OrderDeleteView

--- online_cafe/orders/urls.py
+++ online_cafe/orders/urls.py
@@ -2,11 +2,20 @@
 
 from django.urls import path
 
-from .views import OrderListView, OrderCreateView
+from .views import (
+    OrderCreateView,
+    OrderDeleteView,
+    OrderDetailView,
+    OrderListView,
+    OrderUpdateView,
+)
 
 app_name = "orders"
 
 urlpatterns = [
     path("", OrderListView.as_view(), name="orders-list"),
     path("new/", OrderCreateView.as_view(), name="orders-create"),
+    path("<int:pk>/", OrderDetailView.as_view(), name="orders-detail"),
+    path("<int:pk>/update/", OrderUpdateView.as_view(), name="orders-update"),
+    path("<int:pk>/delete/", OrderDeleteView.as_view(), name="orders-delete"),
 ]
