1a5297451af9c9628d869816b006936fd1d42f00
Vladimir Volosnikov <volosnikovvladimir69@gmail.com>
2025-01-24 19:32:54 +0500

    Add urlpattern for get_revenue_per_shift

--- online_cafe/orders/urls.py
+++ online_cafe/orders/urls.py
@@ -8,12 +8,14 @@ from .views import (
     OrderDetailView,
     OrderListView,
     OrderUpdateView,
+    get_revenue_per_shift,
 )
 
 app_name = "orders"
 
 urlpatterns = [
     path("", OrderListView.as_view(), name="orders-list"),
+    path("revenue/", get_revenue_per_shift, name="orders-revenue"),
     path("new/", OrderCreateView.as_view(), name="orders-create"),
     path("<int:pk>/", OrderDetailView.as_view(), name="orders-detail"),
     path("<int:pk>/update/", OrderUpdateView.as_view(), name="orders-update"),
