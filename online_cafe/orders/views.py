62a319c3e45ec36dbb119f4c4f672e631bf69e51
Vladimir Volosnikov <volosnikovvladimir69@gmail.com>
2025-01-24 13:20:52 +0500

    Add docstrings; add view classes OrderDetailView, OrderUpdateView, OrderDeleteView

--- online_cafe/orders/views.py
+++ online_cafe/orders/views.py
@@ -1,19 +1,60 @@
 """Class with Views."""
 
-from django.views.generic import CreateView, ListView
-from django.urls import reverse_lazy
+from django.urls import reverse, reverse_lazy
+from django.views.generic import (
+    CreateView,
+    DeleteView,
+    DetailView,
+    ListView,
+    UpdateView,
+)
 
 from .models import Order
 
 
 class OrderCreateView(CreateView):
+    """Create view class for Order."""
+
     model = Order
     fields = "table_number", "items"
-    success_url = reverse_lazy("orders:orders_list")
+    success_url = reverse_lazy("orders:orders-list")
     template_name = "orders/orders-create.html"
 
 
 class OrderListView(ListView):
+    """List view class for Order."""
+
     queryset = Order.objects.prefetch_related("items")
     template_name = "orders/orders-list.html"
     context_object_name = "orders"
+
+
+class OrderDetailView(DetailView):
+    """Detail view class for Order."""
+
+    queryset = Order.objects.prefetch_related("items")
+    template_name = "orders/orders-detail.html"
+    context_object_name = "order"
+
+
+class OrderUpdateView(UpdateView):
+    """Update view class for Order."""
+
+    model = Order
+    fields = ("status",)
+    template_name = "orders/orders-update.html"
+
+    def get_success_url(self):
+        """Return success redirect url."""
+        return reverse(
+            "orders:orders-detail",
+            kwargs={"pk": self.object.pk},
+        )
+
+
+class OrderDeleteView(DeleteView):
+    """Delete view class for Order."""
+
+    model = Order
+    success_url = reverse_lazy("orders:orders-list")
+    template_name = "orders/orders-delete.html"
