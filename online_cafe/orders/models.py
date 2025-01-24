83ab8aaf5a69ebb77b695246f564cdd94774a7db
Vladimir Volosnikov <volosnikovvladimir69@gmail.com>
2025-01-24 13:19:47 +0500

    Add class STATUSES, add meta class; add method __str__

--- online_cafe/orders/models.py
+++ online_cafe/orders/models.py
@@ -1,26 +1,42 @@
 """Module with ORM models."""
 
-from django.db import models
 from django.core.validators import MinValueValidator
+from django.db import models
 from menu.models import Dish
 
 
 class Order(models.Model):
     """ORM representation of the table of orders."""
 
-    table_number = models.IntegerField(null=False, verbose_name="Номер стола", validators=[MinValueValidator(0)])
-    items = models.ManyToManyField(Dish, verbose_name="Список блюд в заказе")
-    status = models.CharField(
-        choices={
-            "pending": "в ожидании",
-            "done": "готово",
-            "paid": "оплачено",
-        },
-        verbose_name="Статус заказа",
-        default="в ожидании"
+    class STATUSES(models.TextChoices):
+        """Class with statuses."""
+
+        DONE = "done", "готово"
+        PAID = "paid", "оплачено"
+        PENDING = "pending", "в ожидании"
+
+    table_number: models.IntegerField = models.IntegerField(
+        null=False, verbose_name="Номер стола", validators=[MinValueValidator(0)]
     )
+    items: models.ManyToManyField = models.ManyToManyField(
+        Dish, verbose_name="Список блюд в заказе"
+    )
+    status: models.CharField = models.CharField(
+        choices=STATUSES,
+        verbose_name="Статус заказа",
+        default="в ожидании",
+    )
+
+    class Meta:
+        """Meta class."""
+
+        ordering = ["pk"]
 
     @property
     def total_price(self) -> float:
         """Get total price."""
         return sum(self.items.values_list("price", flat=True))
+
+    def __str__(self) -> str:
+        """Return string representation of the model."""
+        return f"Заказ #{self.pk} - стол №{self.table_number}"
