77058eb662168f1f78bbfeadd3f08d3fb9023ccb
Vladimir Volosnikov <volosnikovvladimir69@gmail.com>
2025-01-24 18:33:47 +0500

    Add view func main_page_view

--- online_cafe/main_page/views.py
+++ online_cafe/main_page/views.py
@@ -1,3 +1,7 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def main_page_view(request: HttpRequest) -> HttpResponse:
+    """View main page."""
+    return render(request, "main_page/main-page.html")
