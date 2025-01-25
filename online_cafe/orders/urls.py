a22299c677949c5b530ce6d1bbee230ad3b32cbd
Vladimir Volosnikov <volosnikovvladimir69@gmail.com>
2025-01-25 12:03:52 +0500

    Register router for OrderViewSet

--- online_cafe/orders/urls.py
+++ online_cafe/orders/urls.py
@@ -1,23 +1,21 @@
 """Module with urlpatterns."""
 
-from django.urls import path
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
 
-from .views import (
-    OrderCreateView,
-    OrderDeleteView,
-    OrderDetailView,
-    OrderListView,
-    OrderUpdateView,
-    get_revenue_per_shift,
-)
+from . import api, views
 
 app_name = "orders"
 
+routers = DefaultRouter()
+routers.register("", api.OrderViewSet)
+
 urlpatterns = [
-    path("", OrderListView.as_view(), name="orders-list"),
-    path("revenue/", get_revenue_per_shift, name="orders-revenue"),
-    path("new/", OrderCreateView.as_view(), name="orders-create"),
-    path("<int:pk>/", OrderDetailView.as_view(), name="orders-detail"),
-    path("<int:pk>/update/", OrderUpdateView.as_view(), name="orders-update"),
-    path("<int:pk>/delete/", OrderDeleteView.as_view(), name="orders-delete"),
+    path("", views.OrderListView.as_view(), name="orders-list"),
+    path("revenue/", views.get_revenue_per_shift, name="orders-revenue"),
+    path("new/", views.OrderCreateView.as_view(), name="orders-create"),
+    path("<int:pk>/", views.OrderDetailView.as_view(), name="orders-detail"),
+    path("<int:pk>/update/", views.OrderUpdateView.as_view(), name="orders-update"),
+    path("<int:pk>/delete/", views.OrderDeleteView.as_view(), name="orders-delete"),
+    path("api/", include(routers.urls)),
 ]
